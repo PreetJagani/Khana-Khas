ecf9fca928cd327fc1528c99d837765e9bd309cf
Preet <preetjagani@gmail.com>
2023-07-24 22:50:21 +0530

    Add animations in onboard screen

--- Khana Khas/Onboard/Controllers/OnboardGetStatedViewController.swift
+++ Khana Khas/Onboard/Controllers/OnboardGetStatedViewController.swift
@@ -8,22 +8,76 @@
 import UIKit
 
 class OnboardGetStatedViewController: UIViewController {
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var getStartLabel: UILabel!
+    @IBOutlet weak var iamgeView: UIImageView!
+    @IBOutlet weak var parentView: UIView!
 
+    @IBOutlet var parentViewWidth: NSLayoutConstraint!
+    @IBOutlet var parentViewLeading: NSLayoutConstraint!
+    
+    var pageVC : OnboardPageViewController?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        for vc in self.children {
+            if vc is OnboardPageViewController {
+                self.pageVC = vc as? OnboardPageViewController
+                self.pageVC?.delegate = self
+            }
+        }
+        self.getStartLabel.alpha = 1
+        self.pageControl.alpha = 0
+        self.iamgeView.alpha = 0
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func showPageControl() {
+        self.parentViewLeading.isActive = false
+        self.parentViewWidth.isActive = true
+        self.parentViewWidth.constant = self.parentView.frame.size.width
+        
+        UIView.animate(withDuration: 0.5) {
+            self.parentViewWidth.constant = 50
+            self.getStartLabel.alpha = 0
+            self.pageControl.alpha = 1
+            self.iamgeView.alpha = 1
+            self.view.layoutIfNeeded()
+        }
     }
-    */
+    
+    func hidePageControl() {
+        
+        self.parentViewWidth.isActive = false
+        
+        UIView.animate(withDuration: 0.5) {
+            self.parentViewLeading.isActive = true
+            self.getStartLabel.alpha = 1
+            self.pageControl.alpha = 0
+            self.iamgeView.alpha = 0
+            self.view.layoutIfNeeded()
+        }
+    }
+}
 
+extension OnboardGetStatedViewController : UIPageViewControllerDelegate {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
+        
+        let vc = pendingViewControllers[0]
+        if vc.title == "1" {
+            self.hidePageControl()
+        } else {
+            self.showPageControl()
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        
+        if let title = pageVC?.viewControllers?[0].title, let index = Int(title) {
+            self.pageControl.currentPage = index - 1
+        }
+        
+    }
 }
--- Khana Khas/Onboard/Controllers/OnboardPageViewController.swift
+++ Khana Khas/Onboard/Controllers/OnboardPageViewController.swift
@@ -11,24 +11,31 @@ class OnboardPageViewController: UIPageViewController {
     
     var myViewControllers : [UIViewController]?
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.delegate = self
         self.dataSource = self
         let storyboard = UIStoryboard(name: "Onboard", bundle: nil)
         let vc1 = storyboard.instantiateViewController(withIdentifier: "vc1")
         vc1.view.backgroundColor = UIColor.purple
+        vc1.title = "1"
+        
         
         let vc2 = storyboard.instantiateViewController(withIdentifier: "vc1")
         vc2.view.backgroundColor = UIColor.red
+        vc2.title = "2"
         
-        myViewControllers = [vc1, vc2]
+        let vc3 = storyboard.instantiateViewController(withIdentifier: "vc1")
+        vc3.view.backgroundColor = UIColor.green
+        vc3.title = "3"
+        
+        myViewControllers = [vc1, vc2, vc3]
     
         self.setViewControllers([vc1], direction: UIPageViewController.NavigationDirection.forward, animated: true)
     }
 }
 
-extension OnboardPageViewController : UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+extension OnboardPageViewController : UIPageViewControllerDataSource {
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         guard let index = self.myViewControllers?.firstIndex(of: viewController), index > 0 else {
